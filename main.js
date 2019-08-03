8a570926737aeb1c0a997e65556e557beec5abb3
Wurui-suck <53506531+Wurui-suck@users.noreply.github.com>
2019-08-04 00:05:26 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+let div1 = document.createElement('div')
+div1.className = 'demo'
+
+document.body.appendChild(div1)
+let dragging = false
+let lastX
+let lsatY
+div1.onmousedown = function (e) {
+    dragging = true
+    lastX = e.clientX
+    lsatY = e.clientY
+}
+
+document.onmousemove = function (e) {
+    if (
+        dragging === true
+    ) {
+        let deltaX = e.clientX - lastX
+        let deltaY = e.clientY - lsatY
+        let top = parseInt(div1.style.top) || 0
+        let left = parseInt(div1.style.left) || 0
+        let resultY = top + deltaY
+        let resultX = left + deltaX
+
+        if (resultY < 0) {
+            resultY = 0
+        }
+        if (resultX < 0) {
+            resultX = 0
+        }
+        div1.style.top = resultY + 'px'
+        div1.style.left = resultX + 'px'
+        lastX = e.clientX
+        lsatY = e.clientY
+
+    }
+    document.onmouseup = function () {
+        dragging = false
+    }
+}
